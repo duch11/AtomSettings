e4c7b862a9a48a7df45bb3242ce0af19f9e5de20
Jonas Holm <jonas@holm.tech>
2016-10-13 16:05:17 +0200

    fixed bat file in order for it to work for future clones

--- FileContainer/copyAtomSettings.bat
+++ FileContainer/copyAtomSettings.bat
@@ -3,23 +3,25 @@
 
 :MENU
 ECHO Atom Copy Batch Script.
-ECHO 1: Load settings (fra git)
-ECHO 2: Save settings (til git)
+ECHO Edit this script in order to make it fit
+ECHO 1: Load settings (Git to Atom)
+ECHO 2: Save settings (Atom to Git)
 ECHO 3: EXIT
 ECHO.
 SET /P M=Indtast valg efterfulgt af ENTER:
 IF %M%==1 GOTO LOAD
 IF %M%==2 GOTO SAVE
+
 IF %M%==3 GOTO EOF
 GOTO MENU
 
 :LOAD
-ROBOCOPY C:\Users\jonas\Desktop\Github\Atom_Settings\FileContainer C:\Users\jonas\.atom  config.cson snippets.cson keymap.cson
+ROBOCOPY C:\Users\jonas\Desktop\Github\AtomSettings\FileContainer C:\Users\jonas\.atom  config.cson snippets.cson keymap.cson
 ECHO git -> atom Done!
 GOTO MENU
 
 :SAVE
-ROBOCOPY C:\Users\jonas\.atom\ C:\Users\jonas\Desktop\Github\Atom_Settings\FileContainer config.cson snippets.cson keymap.cson 
+ROBOCOPY C:\Users\jonas\.atom\ C:\Users\jonas\Desktop\Github\AtomSettings\FileContainer config.cson snippets.cson keymap.cson 
 ECHO atom -> git Done!
 GOTO MENU
 
